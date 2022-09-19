a2e7b77aa0f907bc121d729546ddcd1c2c602ce4
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-09-19 15:13:35 +0200

    Funzione di aggiunta e stampa prodotti

--- Program.cs
+++ Program.cs
@@ -1,4 +1,36 @@
 ﻿// Aggiungi prodotto 
 // rimuovi Prodotto
+Shop s = new Shop();
+bool exit = false;
+do
+{
+    
+    Console.WriteLine("Azioni: ");
+    Console.WriteLine("1.Aggiungi");
+    Console.WriteLine("2.Visualizza");
+    Console.WriteLine("5.Esci");
+    Console.WriteLine("");
 
-Console.WriteLine("shop");
+    Console.WriteLine("Premi il numero per selezionare ");
+
+
+
+    switch (Convert.ToInt32(Console.ReadLine()))
+    {
+
+        case 1:
+            s.AddProduct();
+            Console.Clear();
+            break;
+
+        case 2:
+            s.PrintProducts(s.GetProducts());
+            break;
+
+        default:
+            exit = true;
+            break;
+    }
+
+} while (!exit);
+
--- Shop.cs
+++ Shop.cs
@@ -1,6 +1,60 @@
-﻿// Aggiungi prodotto 
-// rimuovi Prodotto
+﻿
 
 class Shop{
+    Product[] products = new Product[100];
 
+    public Product[] GetProducts()
+    {
+        return products;
+    }
+
+    public string UserString(string message)
+    {
+        Console.WriteLine(message); 
+        string input = Console.ReadLine();
+        return input;
+    }
+
+    public double UserDouble(string message)
+    {
+        Console.WriteLine(message);
+        double input = Convert.ToDouble(Console.ReadLine());
+        return input;
+            
+
+
+    }
+    public void AddProduct()
+    {
+        Product p = new Product(UserString("Inserisci il nome del prodotto"), UserString("Inserisci la descrizione"), UserDouble("Inserisci il prezzo"), 22);
+        for (int i = 0; i < products.Length; i++)
+        {
+            if (products[i] == null)
+            {
+                products[i] = p;
+                break;
+            }
+        }
+    }
+
+    public void PrintProducts(Product[] products)
+    {
+        for (int i = 0; i < products.Length; i++)
+        {
+            if (products[i] != null)
+            {
+                Console.WriteLine ((i + 1) +">>");
+                Console.WriteLine("Nome: " + products[i].GetName());
+                Console.WriteLine("Codice: " + products[i].GetCode());
+                Console.WriteLine("Descrizione: " + products[i].GetDescription());
+                Console.WriteLine("Prezzo: " + products[i].GetPrice());
+                Console.WriteLine("--------");
+                Console.WriteLine();
+
+
+
+            }
+        }
+    }
 }
+
